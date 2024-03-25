613b8843eaf5cb3406b7231f627fbedce1a017b1
nylonmusk <2912rla@gmail.com>
2024-03-25 18:27:04 +0900

    refactor : Builder제거 및 args로 path 주입

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,32 +1,11 @@
-import builder.GatewayBuilder;
 import controller.GatewayController;
-import database.DatabaseManager;
-import dump.Dump;
 
-import filter.FilterFactory;
-import service.ConfigService;
-import service.FilterService;
-import view.Log;
-
-import java.io.IOException;
 import java.text.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ParseException {
-        Log.info(Main.class.getName(), "execute.");
-
-        GatewayController gatewayController = createGatewayController();
+    public static void main(String[] args) throws ParseException {
+        final String configFilePath = args[0];
+        GatewayController gatewayController = new GatewayController(configFilePath);
         gatewayController.execute();
-    }
-
-    private static GatewayController createGatewayController() throws IOException {
-        final String configFilePath = "C:\\Users\\mayfarm\\Documents\\config.json";
-
-        return new GatewayBuilder()
-                .databaseManager(new DatabaseManager())
-                .filterService(new FilterService(new FilterFactory()))
-                .dump(new Dump())
-                .configService(new ConfigService(configFilePath))
-                .build();
     }
 }
