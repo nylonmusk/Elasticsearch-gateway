2ef462392c082b8b4f3e029370b33d463ba71769
nylonmusk <2912rla@gmail.com>
2024-03-25 18:27:38 +0900

    refactor : 원본 데이터 유지로 코드 변경

--- src/main/java/controller/GatewayController.java
+++ src/main/java/controller/GatewayController.java
@@ -1,45 +1,44 @@
 package controller;
 
 import database.DatabaseManager;
-import dump.Dump;
+import dump.DumpBuilder;
+import filter.FilterFactory;
 import service.ConfigService;
 import service.FilterService;
 
-import java.io.IOException;
 import java.text.ParseException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public class GatewayController {
-    private List<Map<String, Object>> fetchedData;
-    private List<Map<String, Object>> filteredData;
-    private final DatabaseManager databaseManager;
+
+    private final FilterFactory filterFactory = new FilterFactory();
+    private final DatabaseManager databaseManager = new DatabaseManager();
+    private final DumpBuilder dumpBuilder = new DumpBuilder();
     private final FilterService filterService;
-    private final Dump dump;
     private final ConfigService configService;
 
-    public GatewayController(DatabaseManager databaseManager, FilterService filterService, Dump dump, ConfigService configService) {
-        this.databaseManager = databaseManager;
-        this.filterService = filterService;
-        this.dump = dump;
-        this.configService = configService;
+    public GatewayController(String configFilePath) {
+        this.configService = new ConfigService(configFilePath);
+        this.filterService = new FilterService(filterFactory);
     }
 
-    public void execute() throws IOException, ParseException {
-        getFetchedData();
-        doFilter();
-        makeDump();
+    public void execute() throws ParseException {
+        List<Map<String, Object>> fetchedData = getFetchedData();
+        List<Map<String, Object>> filteredData = doFilter(fetchedData);
+        makeDump(filteredData);
     }
 
-    private void getFetchedData() {
-        fetchedData = databaseManager.execute(configService.getDatabaseConfig(), configService.getFetchConfig());
+    private List<Map<String, Object>> getFetchedData() {
+        return Collections.unmodifiableList(databaseManager.execute(configService.getDatabaseConfig(), configService.getFetchConfig()));
     }
 
-    private void doFilter() throws IOException, ParseException {
-        filteredData = filterService.filter(fetchedData, configService.getFilterConfig());
+    private List<Map<String, Object>> doFilter(List<Map<String, Object>> fetchedData) throws ParseException {
+        return Collections.unmodifiableList(filterService.filter(fetchedData, configService.getFilterConfig()));
     }
 
-    private void makeDump() {
-        dump.makeJson(filteredData);
+    private void makeDump(List<Map<String, Object>> filteredData) {
+        dumpBuilder.makeJson(filteredData, configService.getDumpConfig());
     }
 }
